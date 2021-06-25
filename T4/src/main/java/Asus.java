5e60e28f6d9a3df8093938ea2c3f791a8f300c8f
AdrianValentinS <83359683+AdrianValentinS@users.noreply.github.com>
2021-06-25 16:51:11 +0300

    .
    .

--- T4/src/main/java/Asus.java
+++
@@ -1,6 +0,0 @@
-public class Asus extends Phone {     // Aici si in superclasa se permite instantierea obiectelor de tip phone si/sau galaxyS6.
-
-    protected Asus(String phoneModel, String phoneColor, String phoneMaterial, String phoneIMEI, int maxBatteryLife) {
-        super(phoneModel, "Asus", phoneColor, phoneMaterial, phoneIMEI, maxBatteryLife);
-    }
-}
--- T4/src/main/java/Zenfone4Max.java
+++
@@ -1,7 +0,0 @@
-public class Zenfone4Max extends Asus{
-
-    public Zenfone4Max(String phoneColor, String phoneMaterial, String phoneIMEI) {
-        super("Zenfone4Max", phoneColor, phoneMaterial, phoneIMEI, 12);
-    }
-
-}
