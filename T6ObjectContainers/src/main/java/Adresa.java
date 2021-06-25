242bb8498320db8e4c7db1bdf8f988a1137fcf71
AdrianValentinS <83359683+AdrianValentinS@users.noreply.github.com>
2021-06-25 17:08:29 +0300

    Re-uploaded assignments
    Assignments reuploaded for future pull request compatibility.

---
+++ T6ObjectContainers/src/main/java/Adresa.java
@@ -0,0 +1,16 @@
+public class Adresa{
+
+    private String adresa;
+
+    public Adresa(String adresa) {
+        super();
+        this.adresa = adresa;
+    }
+
+    @Override
+    public String toString() {
+        return adresa;
+    }
+
+
+}
